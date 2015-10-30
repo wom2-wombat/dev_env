645e0e510862ac291573a124fde14c6896477deb
wom2-wombat <wom2.wombat@gmail.com>
2015-10-30 16:42:25 +0900

    Add a default dotfiles cookbook
    $ chef generate cookbook site-cookbooks/dotfiles

---
+++ site-cookbooks/dotfiles/metadata.rb
@@ -0,0 +1,7 @@
+name 'dotfiles'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures dotfiles'
+long_description 'Installs/Configures dotfiles'
+version '0.1.0'
---
+++ site-cookbooks/dotfiles/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: dotfiles
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
